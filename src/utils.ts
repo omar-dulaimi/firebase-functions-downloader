715918c17e7832533db6e0c12ea7de3fe0f7d779
omar-dulaimi <o.m.dulaimi@gmail.com>
2023-01-30 18:45:04 +0300

    add options param

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,18 @@
+import type { Mode, ObjectEncodingOptions, OpenMode } from 'fs';
 import { promises as fs } from 'fs';
 import path from 'path';
 
-export const writeFileSafely = async (writeLocation: string, content: any) => {
+export const writeFileSafely = async (
+  writeLocation: string,
+  content: any,
+  options?:
+    | (ObjectEncodingOptions & {
+        mode?: Mode | undefined;
+        flag?: OpenMode | undefined;
+      })
+    | BufferEncoding
+    | null,
+) => {
   await fs.mkdir(path.dirname(writeLocation), { recursive: true });
-  await fs.writeFile(writeLocation, content);
+  await fs.writeFile(writeLocation, content, options);
 };
